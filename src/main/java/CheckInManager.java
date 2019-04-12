d1c4d0538e1c94fe3bc82494fe19cb0c7d61404e
Yann <yann.savanchokeo@gmail.com>
2019-04-12 14:24:18 +0200

    Refactor participantCheckInAt9PM case

--- src/main/java/CheckInManager.java
+++ src/main/java/CheckInManager.java
@@ -4,13 +4,14 @@ import java.util.List;
 class CheckInManager {
 
     static int countColdMeals(List<CheckIn> checkInList) {
-        if (checkInList.get(0).getCheckInDate().equals(new Date(2019, 4, 11, 21, 0, 0))) {
+        CheckIn checkIn = checkInList.get(0);
+        Date checkInDate = checkIn.getCheckInDate();
+
+        Date limitTime = new Date(2019, 4, 11, 21, 0, 0);
+        if (checkInDate.equals(limitTime) || checkInDate.after(limitTime)) {
             return 1;
         }
 
-        if (checkInList.get(0).getCheckInDate().equals(new Date(2019, 4, 11, 22, 0, 0))) {
-            return 1;
-        }
         return 0;
     }
 }
