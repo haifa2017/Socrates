3f32a4d82a55743a9ac3d55820ddad6ef772f165
Yann <yann.savanchokeo@gmail.com>
2019-04-12 16:06:48 +0200

    Refactor manyParticipantCheckAtDifferentTime case

--- src/main/java/CheckInManager.java
+++ src/main/java/CheckInManager.java
@@ -4,23 +4,12 @@ import java.util.List;
 class CheckInManager {
 
     static int countColdMeals(List<CheckIn> checkInList) {
-        if (checkInList.size() == 5) {
-            return 2;
-        }
-        CheckIn checkIn = checkInList.get(0);
-        Date checkInDate = checkIn.getCheckInDate();
-
         Date limitTime = new Date(2019, 4, 11, 21, 0, 0);
         Date tomorrow = new Date(2019, 4, 12, 0, 0, 0);
+        long count = checkInList.stream().filter(
+                checkIn -> (checkIn.getCheckInDate().equals(limitTime) || checkIn.getCheckInDate().after(limitTime)) && checkIn.getCheckInDate().before(tomorrow))
+                .count();
 
-        if (checkInDate.equals(tomorrow) || checkInDate.after(tomorrow)) {
-            return 0;
-        }
-
-        if (checkInDate.equals(limitTime) || checkInDate.after(limitTime)) {
-            return 1;
-        }
-
-        return 0;
+        return (int) count;
     }
 }
