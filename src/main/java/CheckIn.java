45475c6cbd76010efe84bdfa3a2390ac9c5eb532
Yann <yann.savanchokeo@gmail.com>
2019-04-15 10:13:37 +0200

    refactor remove getter

--- src/main/java/CheckIn.java
+++ src/main/java/CheckIn.java
@@ -5,15 +5,10 @@ class CheckIn {
     private final LocalDateTime checkInDate;
 
     CheckIn(LocalDateTime checkInDate) {
-
         this.checkInDate = checkInDate;
     }
 
-    public LocalDateTime getCheckInDate() {
-        return this.checkInDate;
-    }
-
     public boolean isEligibleToColdMeal(LocalDateTime limitTime, LocalDateTime tomorrow) {
-        return (getCheckInDate().equals(limitTime) || getCheckInDate().isAfter(limitTime)) && getCheckInDate().isBefore(tomorrow);
+        return (this.checkInDate.equals(limitTime) || this.checkInDate.isAfter(limitTime)) && this.checkInDate.isBefore(tomorrow);
     }
 }
--- src/main/java/CheckInManager.java
+++ src/main/java/CheckInManager.java
@@ -21,5 +21,4 @@ class CheckInManager {
 
         return (int) coldMealsCount;
     }
-
 }
