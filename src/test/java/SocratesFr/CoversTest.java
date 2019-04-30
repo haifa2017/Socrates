79d0078025a901514e573e531710dce74eeaf739
Tristan Lasalle <tristan.lasalle@lacombedulionvert.fr>
2019-04-30 10:13:30 +0200

    refactoring count vegetarian covers case

--- src/test/java/SocratesFr/CoversTest.java
+++ src/test/java/SocratesFr/CoversTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class CoversTest {
 
     @Test
-    public void mealsVegetargian() {
+    public void one_vegetarian_meal_return_one_vegetarian_cover() {
         Meals meals = new Meals(Meal.VEGETARIAN);
 
         Covers covers = meals.asCovers();
@@ -14,4 +14,5 @@ public class CoversTest {
         Covers coverWithOneVegetarianMeal = new Covers(1);
         Assertions.assertThat(covers).isEqualTo(coverWithOneVegetarianMeal);
     }
+
 }
